434af01df217ffdaab28d8b4dc73a7f1a04360ae
Miguel <miguel.viloria.247@gmail.com>
2023-08-19 14:21:14 -0400

    headless false

--- webScapping/bbcNews.ts
+++ webScapping/bbcNews.ts
@@ -1,7 +1,7 @@
 import puppeteer from 'puppeteer';
 
 export const bbcTopNews = async () => {
-    const browser = await puppeteer.launch({});
+    const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     await page.goto('https://www.bbc.com/mundo');
     
